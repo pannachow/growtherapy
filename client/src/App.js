9fc893337b0604bef96147c8a008b3ec2d8cbe45
feliciaboghiu <boghiufelicia@gmail.com>
2020-09-25 12:05:39 -0600

    Scaffolding Complete

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h2>Growtherapy</h2>
+      </div>
+    );
+  }
+}
+export default App;
