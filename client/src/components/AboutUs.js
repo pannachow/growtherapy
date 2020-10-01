1bc9313a224564eafa142c9da9b2e42e67d14c99
pannachow <chowpanna@gmail.com>
2020-10-01 23:06:51 +0200

    update about us page

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -7,14 +7,16 @@ class AboutUs extends React.Component {
 
     return (
       <div className="AboutUs">
+        <br/>
+        <br/>
+        <br/>
         <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
         We are homeplant lovers.
         </Typography>
 
-        <Typography variant="h5"  align="center" color="textSecondary" gutterBottom>
+        <Typography variant="h5"  align="center" color="textSecondary">
         Have fun discovering plants that suit your needs and your environment, get recommendations from us, and start watering your plant today!
         </Typography>
-
         <img src="background.svg" alt="background" />
 
       </div>
