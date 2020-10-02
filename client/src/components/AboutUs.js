5a46ee6ab0f9904bb947cb46679893aa0e886775
feliciaboghiu <boghiufelicia@gmail.com>
2020-10-01 21:56:26 -0600

    ADDED A LIST OF FAQ

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -11,11 +11,15 @@ class AboutUs extends React.Component {
         <br/>
         <br/>
         <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
-        We are homeplant lovers.
+        We are house plant lovers.
         </Typography>
 
         <Typography variant="h5"  align="center" color="textSecondary">
-        Have fun discovering plants that suit your needs and your environment, get recommendations from us, and start watering your plant today!
+        Plant therapy – it might sound like a new fad, but in fact, the practice is ancient. Engaging with plants can have a relaxing, peaceful effect on many people. Have fun discovering plants that suit your needs and your environment, get recommendations from us, and start watering your plant today!
+        </Typography>
+
+        <Typography variant="subtitle1">
+          WOW nice
         </Typography>
         <img src="background.svg" alt="background" />
 
