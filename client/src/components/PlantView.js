9d4c3ca6af5b3d8b0d5a097f2f5937e10fb5f8c2
pannachow <chowpanna@gmail.com>
2020-10-15 17:34:15 +0200

    fetch plant from backend in plantview

--- client/src/components/PlantView.js
+++ client/src/components/PlantView.js
@@ -1,31 +1,56 @@
 import React from 'react';
+import { withRouter } from "react-router-dom";
 import Container from '@material-ui/core/Container';
 import Calendar from './Calendar';
 import Typography from '@material-ui/core/Typography';
 
-const plantData = {
-  name: "Lemon Butter Fern",
-  sun: "Your fern prefers bright indirect light. Find a south-facing window or the brightest alternative.",
-  water: "Regular weekly watering is encouraged, and avoid drought conditions. Keep the soil moist but not soggy.",
-  tips: "Bring your fern off the floor or table by using a plant stand or a plant hanger, or by placing it on a tall surface, to encourage the sprawling, trailing foliage to flourish.",
-  wateringInterval: 3,
-};
+const sunlight = {
+  1: "Low to indirect sunlight.",
+  2: "Bright indirect sunlight.",
+  3: "Full direct sunlight."
+}
 
-const wateringSchedule = {
-  start: new Date(),
-  interval: plantData.wateringInterval,
-  text: "Time to water this bad boy",
-};
+const watering = {
+  1: "Water thoroughly once a month.",
+  2: "Water thoroughly every 2 - 3 weeks.",
+  3: "Water thoroughly once a week."
+}
 
-export default class PlantView extends React.Component {
+class PlantView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      plantData: null
+    };
+  }
+
+  // Fetch plantData from backend
+  async componentDidMount() {
+    const result = await fetch("http://localhost:5000/plants/1258091");
+    // const result = await fetch(`http://localhost:5000/plants/${this.props.match.params.id}`);
+    const data = await result.json();
+    this.setState({plantData: data})
+  } 
+
   render() {
+    const plantData = this.state.plantData;
+    if (!plantData) {
+      return <Container>No plant data found.</Container>
+    }
+
+    const wateringSchedule = {
+      start: new Date(),
+      interval: plantData.growtherapy.water_needs * 7,
+      text: "Time to water this bad boy",
+    };
+
     return (
       <Container>
         <br />
         <br />
 
         <div style={{ display: "grid", gridTemplateColumns: "50% 50%" }}>
-          <img src="plant-view.svg" alt="plant" />
+          <img src={plantData.growtherapy.image_url} alt="plant" style={{width: "90%"}}/>
           <div style={{ display: "grid", gridTemplateRows: "50% 50%" }}>
 
             <div style={{ display: "grid", gridTemplateColumns: "auto auto", columnGap: "5px" }}>
@@ -34,28 +59,35 @@ export default class PlantView extends React.Component {
                 Name:
               </Typography>
               <Typography color="secondary" variant="body1">
-                {plantData.name}
+                {plantData.common_name}
               </Typography>
 
               <Typography color="primary" variant="body1">
-                Sun:
+                Family:
               </Typography>
               <Typography color="secondary" variant="body1">
-                {plantData.sun}
+                {plantData.family}
               </Typography>
 
               <Typography color="primary" variant="body1">
-                Water:
+                Water Needs:
               </Typography>
               <Typography color="secondary" variant="body1">
-                {plantData.water}
+                {watering[plantData.growtherapy.water_needs]}
               </Typography>
 
               <Typography color="primary" variant="body1">
-                Tips:
+                Light Needs:
               </Typography>
               <Typography color="secondary" variant="body1">
-                {plantData.tips}
+                {sunlight[plantData.growtherapy.light_needs]}
+              </Typography>
+
+              <Typography color="primary" variant="body1">
+                Notes:
+              </Typography>
+              <Typography color="secondary" variant="body1">
+                {plantData.growtherapy.notes}
               </Typography>
             </div>
 
@@ -67,3 +99,5 @@ export default class PlantView extends React.Component {
     )
   }
 }
+
+export default withRouter(PlantView);
